48551f37896dafe97efc292117ac9cce5b9e6052
cuihaipeng <cuihp@weather.com.cn>
2020-01-17 12:02:13 +0800

    限制project长度不超过10位汉字或20位字母

--- agent/common/exceptions/__init__.py
+++ agent/common/exceptions/__init__.py
@@ -7,6 +7,7 @@
 @pyVersion: 3.6.8
 @desc :
 """
+import re
 
 
 class NullValueError(Exception):
@@ -37,6 +38,20 @@ class TooLongError(Exception):
         return f'你输入的{self.key}长度是{self.length}, 不能超过{self.max_len}个字符'
 
 
+class MixtureStrError(Exception):
+    """自定义异常类，继承Exception
+            值不能为空
+        """
+
+    def __init__(self, max_str_len, key=None):
+        self.key = key
+        self.max_str_len = max_str_len
+
+    def __str__(self):
+        """设置抛出异常的描述信息"""
+        return f'请检查你输入的{self.key}是否正确，请输入{self.max_str_len}位汉字或{self.max_str_len * 2}位字母'
+
+
 def dict_value_is_empty(_dict, key):
     if len(str(_dict.get(key)).strip()) == 0:
         raise NullValueError(key)
@@ -46,6 +61,13 @@ def dict_value_is_empty(_dict, key):
 
 def str_length_valid(value, max_len, key=None):
     if len(value) > max_len:
-        raise TooLongError(len(value), max_len, key)
+        raise TooLongError(len(str(value)), max_len, key)
     else:
         return value
+
+
+def mixture_length_valid(value, strLength, key=None):
+    pattern = "^[\u4e00-\u9fa5]{1," + str(strLength) + "}$|^[\dA-Za-z]{1," + str(strLength * 2) + "}$"
+    var = re.match(pattern, str(value))
+    if var is None:
+        raise MixtureStrError(strLength, key)
