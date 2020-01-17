332e1a139081eca964e04ed1c89a10305fe3dc83
cuihaipeng <cuihp@weather.com.cn>
2020-01-17 11:08:41 +0800

    aaa

--- agent/http/__init__.py
+++ agent/http/__init__.py
@@ -1,15 +1,27 @@
 # coding: utf-8
+'''
+@author: lb
+@file: api_agent.py
+@time: 1/14/2020 2:03 PM
+@desc:
+@Version: v1.0
+'''
 
-"""
-@Time : 2020-01-10 17:10 
-@Author : cuihaipeng
-@File : __init__.py.py
-@pyVersion: 3.6.8
-@desc :
-"""
+# from agent.common import gol, agent_push, constant
+# from agent.conf.configs import config
+# from agent.conf.config_read import ServerConf
+# from agent.conf.config_read import ConfigInit
+# from agent.http import url_access
 
+import os
+import sys
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(BASE_DIR)
 from agent.common import gol, agent_push, constant
 from agent.conf.configs import config
+from agent.conf.config_read import ServerConf
+from agent.conf.config_read import ConfigInit
 from agent.http import url_access
 
 
@@ -36,18 +48,29 @@ def api_agent(http_dict, server_dict):
     value = result[0]
     sender = agent_push.AgentPush(endpoint, metric, step, value, "GAUGE", tags)
     # 数据有更新，尝试连续推送5次，其他情况只推送一次
+    success_count = 0
+    range_times = 1
     if value == constant.const.UPDATE_CODE:
-        for _ in range(constant.const.UPDATE_PUSH_TIME):
-            sender.push(push_url)
-    sender.push(push_url)
+        range_times += constant.const.UPDATE_PUSH_TIME
+    for _ in range(range_times):
+        if sender.push(push_url):
+            success_count += 1
+    if success_count == 0:
+        raise RuntimeError("Failed to send report to Falcon!")
+
+
 
 
 def main():
     gol.init()
     http_list = config.get('http')
-    server_dict = config.get('server')
-    for http_dict in http_list:
-        api_agent(http_dict, server_dict)
+    server_zone = config.get('server')
+    if http_list is not None and server_zone is not None:
+        server_dict = ServerConf(server_zone)
+        for http_zone in http_list:
+            http_dict = ConfigInit(http_zone)
+            api_agent(http_dict, server_dict)
+
 
 
 if __name__ == "__main__":
